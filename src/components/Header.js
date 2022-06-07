a0387e3109dbdb0eecc83578dd408f49079002bb
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2022-06-07 20:34:24 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom'
+import Contact_us from './src1/Contactus.js';
 import logo from "./adroidlogo.png"
 function Header(){
     return(
+<Router>
 
         <nav className="navbar bg-dark">
   <div className=" mt-2 mb-2 container "style={{  marginRight:0} }>
@@ -9,11 +15,16 @@ function Header(){
       </a>
     <div className="float-end mt-2 mb-2 " >
 
-        <button className=" btn btn-danger rounded-0 btn-lg  "    >contact us</button>
+        <Link className=" btn btn-danger rounded-0 btn-lg  "  to="/contact"  >contact us</Link>
     </div>
     
   </div>
 </nav>
+<Routes>
+          <Route path="/contact" element={<Contact_us />} />
+
+        </Routes>
+</Router>
         )
 }
 export default Header;  
