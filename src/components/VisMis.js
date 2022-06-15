03a8f0751c74c58d54f92ab88efc819471b87c2b
Taha varda <tahavarda52@gmail.com>
2022-06-15 12:12:54 +0530

    bg color for VisMis

--- src/components/VisMis.js
+++ src/components/VisMis.js
@@ -3,14 +3,14 @@ function Vismis(){
     return(
         <div className="mb-5" style={{overflow:"hidden",backgroundImage: `url(${write})`}}>
             <div className="float-start ms-5 ps-3 pt-5 pb-3" style={{marginRight:"30%"}}>
-                <h1 className="pt-5">Our  <mark style={{color:"red", backgroundColor:"white"}}>
+                <h1 className="pt-5">Our  <mark style={{color:"red", backgroundColor:"none"}}>
 
 Vision
 </mark></h1>
                 <p>To provide an ecosystem to excel the student community, being a thinker, innovator or implementer, creates a comfortable zone for students and mentors to discuss about the research, startup or counselling.</p>
             </div>
             <div className="float-end me-5 pe-3 pt-5 pb-5" style={{marginLeft:"30%"}}>
-                <h1 className="float-end pe-5">Our  <mark style={{color:"red", backgroundColor:"white"}}>
+                <h1 className="float-end pe-5">Our  <mark style={{color:"red", backgroundColor:"none"}}>
 
 Mission    
 </mark></h1>
