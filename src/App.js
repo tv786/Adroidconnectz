fc46005e581243ce8241d1fbdb513a4e0477445c
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2022-06-15 12:22:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import Header from "./components/Header";
-import Navbar from "./components/navbar";
-import Footer from "./components/Footer";
-import Carousel1 from "./components/Carousel1";
-import Carousel2 from "./components/Carousel2";
-import Img2 from "./components/img2"
-import Aboutus from "./components/aboutus"
-import Vismis from "./components/VisMis";
-import Lower from "./components/Lower";
-import { BrowserRouter } from 'react-router-dom';
+
+import {
+BrowserRouter as Router,
+
+Route,
+Routes
+
+} from "react-router-dom";
+
+// import Home component
+import Home from "./components/Home.js";
+import Contact_us from './components/Contactus.js';
+import Features from "./components/features.js"
+
 
 
 function App() {
+return (
+	<>
+	{/* This is the alias of BrowserRouter i.e. Router */}
+	<Router>
+		<Routes>
+		{/* This route is for home component
+		with exact path "/", in component props
+		we passes the imported component*/}
+		<Route exact path="/" element={<Home/>} />
+		<Route path="/features" element={<Features/>} />
 
-  return (
-    
-    <div>
+	
+		<Route path="/contact" element={<Contact_us/>} />
+			
+		{/* If any route mismatches the upper
+		route endpoints then, redirect triggers
+		and redirects app to home component with to="/" */}
 
-
-    <Header />
-    <Navbar />
-    <Carousel1 />
-    <Carousel2 />
-
-
-
-   <Vismis />
-    <Img2 />
-
-
-<Aboutus />
-<Lower />
-    <Footer />
-
-
-    </div>
-    );
+		</Routes>
+	</Router>
+	</>
+);
 }
-
 
 export default App;
