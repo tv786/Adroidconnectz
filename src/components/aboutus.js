8cf66097466de6bbd097e8fe96673cc46c6bbeaf
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2022-06-18 21:03:38 +0530

    Update aboutus.js

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -7,15 +7,18 @@ function Aboutus() {
         backgroundImage: `url(${bg1}), 
         url(${red})
          `,
+         paddingBottom:"500px",
          backgroundBlendMode:"multiply",
-         color:"white",
+         fontSize : "14px",
 
+         color:"white",
+        //  opacity:"0.75"
 
       }}
     >
 
       <h1 style={{ textAlign: "center",fontSize:"40px" }} >About Us</h1>
-      <div className="border mt-5 mb-5 pt-4 pb-4 pe-4 ps-4" style={{ position:"" ,marginLeft: "10%", marginRight: "10%" ,fontSize:"18px",lineHeight:"2"   }}>
+      <div className="border mt-5 mb-5 pt-4 pb-4 pe-4 ps-4" style={{ position:"" ,marginLeft: "10%", marginRight: "10%" ,fontSize:"18px",lineHeight:"2" ,        backgroundImage: `url(${red})`  }}>
         <p>
           Adroid Connectz is the nation's biggest community of students and
           mentors , providing the one stop solution to all the queries related
