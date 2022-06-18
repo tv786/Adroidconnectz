46bc5d59bafd053141587c5bea2eafd268e3e5a8
JATIN-GUPTA-2108 <63347186+JATIN-GUPTA-2108@users.noreply.github.com>
2022-06-18 20:52:02 +0530

    Add files via upload

--- src/components/features/components/cards.js
+++ src/components/features/components/cards.js
@@ -2,11 +2,12 @@ import students from "./students.jpeg";
 function Cards(props){
     return(
 
-        <div class="card col m-5 border-0 p-3  " style={{ width: "15rem" ,
+        <div class="card col m-5 border-0 h-100 " style={{ 
+                // width: "15rem" ,
         backgroundColor:"black" ,color:"white"
         }}
         >
-<img src={props.src} class="card-img-top" alt="..." />
+<img src={props.src} class="card-img-top" alt="..."  width="100px" height="200px"/>
 <div className="card-body">
 <h6>{props.heading}</h6>
   <p className="card-text">{props.text} </p>
