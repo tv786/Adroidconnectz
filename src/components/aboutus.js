385a2acafbd503097f96fd926baa5a7a8ce2da7e
Taha varda <tahavarda52@gmail.com>
2022-06-20 16:49:14 +0530

    packageLock

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -20,14 +20,14 @@ function Aboutus() {
       <h1 style={{ textAlign: "center",fontSize:"40px" }} >About Us</h1>
       <div className="border mt-5 mb-5 pt-4 pb-4 pe-4 ps-4" style={{ position:"" ,marginLeft: "10%", marginRight: "10%" ,fontSize:"18px",lineHeight:"2" ,        backgroundImage: `url(${red})`  }}>
         <p>
-          Adroid Connectz is the nation's biggest community of students and
+          Adroidconnectz is the nation's biggest community of students and
           mentors , providing the one stop solution to all the queries related
           to startups , research , internships , higher studies ,innovations
           etc.
         </p>
 
         <p>
-          Team of Adroid Connectz comprises of renowned ressearches and
+          Team of Adroidconnectz comprises of renowned ressearches and
           enterpreneurs . This team motivates the students to undertake a career
           exploration journey and make appropriate career choices at right time.
         </p>
